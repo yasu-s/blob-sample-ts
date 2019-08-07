55ab88d0755d043519977b982966438d14980572
Yasuyuki Saito <yasuyuki.saito.2012@gmail.com>
2019-08-07 20:43:12 +0900

    ファイルDL処理追加

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,23 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'ng-sample';
+  /** data */
+  text = '';
+
+  /**
+   * data save
+   */
+  save(): void {
+    if (!this.text) { return; }
+
+    const blob = new Blob([this.text], { type: 'text/plain' });
+
+    const a = document.createElement('a');
+    a.href =  URL.createObjectURL(blob);
+    a.download = 'sample.txt';
+    a.click();
+  }
 }
